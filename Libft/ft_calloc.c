6694daa4cf8be9b3be611b7c49a4cfd6f4a1ed6e
Gabriel Vivas <136252761+gavivas-null@users.noreply.github.com>
2024-07-30 22:25:48 +0200

    calloc arreglado

--- Libft/ft_calloc.c
+++ Libft/ft_calloc.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_calloc.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gavivas- <gavivas-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: gabo <gabo@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/09 17:13:57 by gavivas-          #+#    #+#             */
-/*   Updated: 2024/03/09 18:42:01 by gavivas-         ###   ########.fr       */
+/*   Updated: 2024/07/30 22:17:26 by gabo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,25 @@ void	*ft_calloc(size_t count, size_t size)
 	size_t	totalsize;
 
 	totalsize = count * size;
+	if ((SIZE_MAX / size) < count && size != 0)
+		return (NULL);
 	arr = (void *)malloc(totalsize);
 	if (arr == NULL)
 		return (NULL);
 	ft_bzero(arr, size * count);
 	return (arr);
 }
+
+/* int main()
+{
+	size_t count = SIZE_MAX;
+	size_t size = 5;
+
+	void *arr = ft_calloc(count, size);
+	if (arr == NULL)
+		printf("Error\n");
+	else
+		printf("Bien Chaval.\n");
+	if (arr != NULL)
+		free(arr);
+} */
